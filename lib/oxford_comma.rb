eda904c27febd719df93fe9de1804c7efba8d167
Nicholas Keller <nic.keller328@gmail.com>
2020-03-26 18:16:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+ new_array = []
+ number = array.length
+ counter = 0
+ long_list = ""
+ 
+ if number == 1
+   array.join
+   elsif number == 2
+    array.join(" and ")
+    elsif number >=3
+      while counter < number-1
+        long_list << array[counter] + ", "
+        counter += 1
+      end
+      long_list << "and " + array[-1]
+    else
+ end
 end
