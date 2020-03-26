43117a6a29b659d7a3cf1bd829f7694b7d61a4d1
Nicholas Keller <nic.keller328@gmail.com>
2020-03-26 17:55:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-  new_array = array.split(", ")
+ 
+ final_array = []
+ number = array.length
+ 
+ if number == 1
+   array.split
+   elsif number == 2
+    array.split(" and ")
+  else
+ end
+ 
+ 
+ 
 end
