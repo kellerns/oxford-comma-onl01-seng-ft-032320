759deee28d6fbac2721f1a1cd9dd169975f16257
Nicholas Keller <nic.keller328@gmail.com>
2020-03-26 17:57:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,9 @@ def oxford_comma(array)
  number = array.length
  
  if number == 1
-   array.split
+   array.join
    elsif number == 2
-    array.split(" and ")
+    array.join(" and ")
   else
  end
  
